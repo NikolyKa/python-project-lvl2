33a62ec59cc73e837d1d7dd6ba5beb401853c40d
NikolyKa <karabakin.kolia@mail.ru>
2022-08-04 13:04:03 +0300

    add stylish

--- gendiff/diff.py
+++ gendiff/diff.py
@@ -2,8 +2,10 @@ from gendiff.engine.data_extractor import get_data
 from gendiff.engine.engine import collect_diff_segments
 from gendiff.engine.stylish import stylish
 
+styles = {'stylish':stylish}
+
 
 def generate_diff(first_file, second_file, form='stylish'):
     first_dict, second_dict = get_data(first_file, second_file)
-    diff = stylish(collect_diff_segments(first_dict, second_dict))
+    diff = styles[form](collect_diff_segments(first_dict, second_dict))
     return diff
