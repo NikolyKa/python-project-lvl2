d5ccdceba58cd2ae9e32f13a091535c2a10480f6
NikolyKa <karabakin.kolia@mail.ru>
2022-08-09 10:57:23 +0300

    add new changes

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -1,6 +1,17 @@
 import json
 
 
+def convert(dictionary, depth):
+    indent = "    " * depth
+    res = '{\n'
+    keys = dictionary.keys()
+    for key in keys:
+        value = dictionary.get(key)
+        res += f'{indent}    {key}: ' + values(value, depth)
+    res += indent + '}\n'
+    return res
+
+
 def stylish(dict_):
     depth = 0
     result = '{\n'
@@ -13,21 +24,21 @@ def stylish(dict_):
             status, value = child['status'], child['value']
             results = ''
             if status == 'nested':
-                results += f'{indent}    {key}:' + ' {\n'
+                results += f'{indent}    {key}: ' + '{\n'
                 for k, v in value.items():
                     results += converter(k, v, depth + 1)
                 results += indent + "    " + '}\n'
             elif status == 'changed':
                 val1 = value[0]
                 val2 = value[1]
-                results += f'{indent}  - {key}:' + values(val1, depth)
-                results += f'{indent}  + {key}:' + values(val2, depth)
+                results += f'{indent}  - {key}: ' + values(val1, depth)
+                results += f'{indent}  + {key}: ' + values(val2, depth)
             elif status == 'added':
-                results = f'{indent}  + {key}:' + values(value, depth)
+                results = f'{indent}  + {key}: ' + values(value, depth)
             elif status == 'deleted':
-                results = f'{indent}  - {key}:' + values(value, depth)
+                results = f'{indent}  - {key}: ' + values(value, depth)
             elif status == 'unchanged':
-                results = f'{indent}    {key}:' + values(value, depth)
+                results = f'{indent}    {key}: ' + values(value, depth)
             return results
 
         result += converter(key, child, depth)
@@ -38,21 +49,8 @@ def stylish(dict_):
 def values(value, depth):
     if isinstance(value, dict):
         return convert(value, depth + 1)
-    elif len(str(value)) == 0:
-        return '\n'
     else:
         if not isinstance(value, str):
-            return f' {json.dumps(value)}\n'
+            return f'{json.dumps(value)}\n'
         else:
-            return f' {value}\n'
-
-
-def convert(dictionary, depth):
-    indent = "    " * depth
-    res = ' {\n'
-    keys = dictionary.keys()
-    for key in keys:
-        value = dictionary.get(key)
-        res += f'{indent}    {key}:' + values(value, depth)
-    res += indent + '}\n'
-    return res
+            return f'{value}\n'
